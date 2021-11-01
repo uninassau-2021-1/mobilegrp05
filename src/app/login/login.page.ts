05bd7b8e776d1207d67909db73eb6c0be0c497f1
Thiago Thomas <thiago_rocha17@hotmail.com>
2021-11-01 17:59:04 -0300

    Finalizado o login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 import { Platform } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,22 +10,27 @@ import { Platform } from '@ionic/angular';
 })
 export class LoginPage {
 
-  constructor(public http:HttpClient,public platform:Platform) { }
+  constructor(public http:HttpClient,public platform:Platform, private route: Router) { }
 
-  private usuario;
-  private senha;
+  private usuario: string = "";
+  private senha: string = "";
 
   Login() {
     console.log(this.usuario);
     console.log(this.senha);
     //var dadosParaEnviar = {usuario: "thiago", senha:"123"}
-    var url = "http://localhost:3000/login?usuario=thiago&senha=123";
+    //var url = "http://localhost:3000/login?usuario=thiago&senha=123";
     //this.http.get(url)
 
-    this.http.get(url).subscribe(data => {
-      console.log(data);
+
+    this.http.get(`http://localhost:3000/login?usuario=${this.usuario}&senha=${this.senha}`).subscribe((response) => {
+      if(!response[0]) {
+        alert("Senha Incorreta!");
+      }else {
+        this.route.navigate(['/tabs']);
+      };
     });
-    
+
   }
 
 }
